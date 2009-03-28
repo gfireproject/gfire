ecf7f59d992fc411fa797b4dc3f2780894f7ffbd
laurentdemarez <me@laurntdemarez.com>
2009-03-28 21:20:43 +0000

    autogen script improved
    git-svn-id: https://my-svn.assembla.com/svn/gfire/trunk@44 e0e46c49-be69-4f5a-ad62-21024a331aea

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,16 @@
-aclocal && \
-automake -a -c && \
-autoheader && \
-autoconf
+#! /bin/sh
+echo "Generating configuration files for Gfire, please wait...."
+echo;
+
+echo "Running libtoolize, please ignore non-fatal messages...."
+echo n | libtoolize --copy --force || exit;
+echo;
+
+aclocal || exit;
+autoheader || exit;
+automake --add-missing --copy
+autoconf || exit;
+automake || exit;
+
+echo "Done."
 
