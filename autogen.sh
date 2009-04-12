7f630ce1da4923d519ffa41f893d628504a9c790
nwarrenfl <nwarrenfl@gmail.com>
2009-04-12 19:26:51 +0000

    Update autotools scripts and improve them
    git-svn-id: https://my-svn.assembla.com/svn/gfire/trunk@79 e0e46c49-be69-4f5a-ad62-21024a331aea

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,15 @@
-#! /bin/sh
-echo "Generating configuration files for Gfire, please wait...."
+#!/bin/sh
+echo "Generating configuration files for Gfire, please wait..."
 echo;
 
-echo "Running libtoolize, please ignore non-fatal messages...."
+echo "Running libtoolize, please ignore non-fatal messages."
 echo n | libtoolize --copy --force || exit;
-echo;
 
 aclocal || exit;
 autoheader || exit;
-automake --add-missing --copy
+automake --add-missing --copy || exit;
 autoconf || exit;
 automake || exit;
 
+echo;
 echo "Done."
-
